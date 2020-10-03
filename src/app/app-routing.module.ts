409876d439e576ab1e1035c340cfec8bf0f52f62
febys <napsterfly@gmail.com>
2020-10-03 21:00:33 +0200

    first commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,24 @@
-import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import { NgModule } from "@angular/core";
+import { Routes, RouterModule } from "@angular/router";
+import { AdminBookComponent } from "./admin/admin-books/admin-book/admin-book.component";
+import { BookFormComponent } from "./admin/book-form/book-form.component";
+import { BookComponent } from "./book/book/book.component";
+import { HomeComponent } from "./home/home.component";
+import { LoginComponent } from "./log-in/login/login.component";
+import { SignUpComponent } from "./sign-up/sign-up/sign-up.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: "", component: HomeComponent },
+  { path: "book", component: BookComponent },
+  { path: "login", component: LoginComponent },
+  { path: "signup", component: SignUpComponent },
+  { path: "admin/books/new", component: BookFormComponent },
+  { path: "admin/books/:id", component: AdminBookComponent },
+  { path: "admin/books", component: AdminBookComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,35 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
+import { HttpClientModule } from "@angular/common/http";
+import { AppRoutingModule } from "./app-routing.module";
+import { AppComponent } from "./app.component";
+import { HeaderComponent } from "./header/header.component";
+import { HomeComponent } from "./home/home.component";
+import { RouterModule } from "@angular/router";
+import { FormsModule } from "@angular/forms";
+import { BookComponent } from "./book/book/book.component";
+import { LoginComponent } from "./log-in/login/login.component";
+import { SignUpComponent } from "./sign-up/sign-up/sign-up.component";
+import { AdminBookComponent } from "./admin/admin-books/admin-book/admin-book.component";
+import { BookFormComponent } from "./admin/book-form/book-form.component";
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    HomeComponent,
+    //RouterModule,
+    // HttpClientModule,
+    //  FormsModule,
+    //AppRoutingModule,
+    BookComponent,
+    LoginComponent,
+    SignUpComponent,
+    AdminBookComponent,
+    BookFormComponent,
   ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  imports: [BrowserModule, AppRoutingModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
