cb0a91e2d58e37951403504ce574d443ebe30184
febys <napsterfly@gmail.com>
2020-10-10 14:12:02 +0200

    commit-5

--- src/app/book/admin-books/admin-books.component.ts
+++ src/app/book/admin-books/admin-books.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from "@angular/core";
 import { Router } from "@angular/router";
+import { Subject } from "rxjs";
 import { BookEntity } from "../modules/booksTdo";
 import { BookService } from "../services/book.service";
 
@@ -15,29 +16,40 @@ export class AdminBooksComponent implements OnInit {
   editMode: false;
   title = "angulardatatables";
   dtOptions: DataTables.Settings = {};
-  constructor(private bookService: BookService, private router: Router) {
+  dtTrigger: Subject<BookEntity> = new Subject();
+  constructor(private bookService: BookService, private router: Router) {}
+  ngOnInit() {
+    this.bookService.getData;
+
+    this.dtOptions = {
+      pagingType: "full_numbers",
+      pageLength: this.length,
+      processing: true,
+      lengthChange: true,
+    };
     this.bookService.getDataList({ fillFieldLabels: true }).subscribe(
       (res) => {
         this.data = res.data.list;
         this.length = this.data.length;
 
-        this.dtOptions = {
-          pagingType: "full_numbers",
-          pageLength: this.length,
-          processing: false,
-          lengthChange: true,
-        };
+        console.log(this.dtOptions);
+        console.log(this.length);
+        this.dtTrigger.next();
       },
 
       () => console.log("Complete")
     );
+    //  this._getDataList();
     this.bookService.getMetaData().subscribe((res) => {
       this.bookMetadata = res.data.fieldMap;
       // console.log(this.bookMetadata);
     });
   }
-  ngOnInit() {}
-
+  private _getDataList(): void {}
+  ngOnDestroy(): void {
+    // Do not forget to unsubscribe the event
+    this.dtTrigger.unsubscribe();
+  }
   onDelete(id: number) {
     this.bookService.deleteData(id).subscribe((value) => {
       // console.log(value);
--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -20,52 +20,49 @@ export class HomeComponent implements OnInit {
     private bookService: BookService,
     private route: ActivatedRoute,
     public dialog: MatDialog
-  ) {}
-
-  ngOnInit() {
-    this._getDataList();
-    console.log(this.data);
+  ) {
     this.route.params.subscribe((params: Params) => {
       this.id = +params["id"];
       this.boxMode = params["id"] != null;
       this.openDialog();
     });
-  }
-  public openDialog() {
-    const dialogConfig = new MatDialogConfig();
-    dialogConfig.disableClose = false;
-    dialogConfig.autoFocus = true;
-    if (this.boxMode) {
-      this.bookService.getData(this.id, this.boxMode).subscribe((value) => {
-        this.idF = value.id;
-        this.dataF = value;
-        console.log(this.idF);
-        console.log(this.dataF.data.title);
-
-        this.dialog.open(DialogComponent, {
-          data: {
-            title: this.dataF.data.title,
-            author: this.dataF.data.author,
-            description: this.dataF.data.description,
-            category: this.dataF.data.category,
-            price: this.dataF.data.price,
-            image: this.dataF.data.image,
-            nr: this.dataF.data.id,
-          },
-        });
-      });
-    }
-  }
-  private _getDataList(): void {
     this.bookService.getDataList({ fillFieldLabels: true }).subscribe(
       (res) => {
         this.data = res.data.list;
-
         this.length = res.data.totalRecords;
+
         console.log(this.data.id);
       },
 
       () => console.log("Complete")
     );
+  }
+
+  ngOnInit() {
+    console.log(this.data);
+  }
+  public openDialog() {
+    const dialogConfig = new MatDialogConfig();
+    dialogConfig.disableClose = false;
+    dialogConfig.autoFocus = true;
+
+    this.bookService.getData(this.id, this.boxMode).subscribe((value) => {
+      this.idF = value.id;
+      this.dataF = value;
+      console.log(this.idF);
+      console.log(this.dataF.data.title);
+
+      this.dialog.open(DialogComponent, {
+        data: {
+          title: this.dataF.data.title,
+          author: this.dataF.data.author,
+          description: this.dataF.data.description,
+          category: this.dataF.data.category,
+          price: this.dataF.data.price,
+          image: this.dataF.data.image,
+          nr: this.dataF.data.id,
+        },
+      });
+    });
   }
 }
